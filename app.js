08499c578f2fe090cc586e1371d8de90de135628
Guido Perdomo <guidop91@gmail.com>
2021-12-23 11:34:30 -0400

    add a route

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-'use strict';
-
-// [START gae_node_request_example]
 const express = require('express');
 const winston = require('winston')
 
@@ -32,6 +15,12 @@ app.use(logError)
 app.get('/', (req, res) => {
   res.status(200)
   .send('<h1>Hello, world!</h1><p>It\'s nice to meet you!</p>')
+  .end();
+});
+
+app.get('/me', (req, res) => {
+  res.status(200)
+  .send('<h1>So this is me!</h1><p>And you cannot deny it!</p>')
   .end();
 });
 
